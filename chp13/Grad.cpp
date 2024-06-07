29b630acbd548b54205f2ea001d02e4195ce2faf
SeBeom12 <sebeom6135@naver.com>
2024-06-07 12:20:27 +0900

    chp13

---
+++ chp13/Grad.cpp
@@ -0,0 +1,20 @@
+#include "Grad.h"
+#include <algorithm>
+#include "grade.h"
+
+
+istream& read_hw(istream& in, vector<double>& hw);
+
+istream& Grad::read(istream& in) {
+	Core::read_common(in);
+	in >> thesis;
+	read_hw(in, Core::homework);
+	return in;
+
+}
+
+double Grad::grade() const {
+	return min(Core::grade(), thesis);
+
+
+}
---
+++ chp13/grade.h
@@ -0,0 +1,19 @@
+#pragma once
+// grade.h
+
+#ifndef GUARD_grade_h
+#define GUARD_grade_h
+
+#include <list>
+#include <vector> // vector -> Vec
+
+
+
+
+double grade(double, double, double);
+double grade(double, double, std::vector<double>&);
+
+
+
+
+#endif
